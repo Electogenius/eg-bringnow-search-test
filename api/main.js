367b52ec3791b6b9347fec85f4587098b40d9db4
liimee <alt3753.7@gmail.com>
2021-04-21 23:22:20 +0700

    😶

--- api/main.js
+++ api/main.js
@@ -7,7 +7,7 @@ export default (req) => {
       if (response.ok) return response.json()
       throw new Error('err')
     }).then((a) => {
-      var data = JSON.parse(a);
+      var data = a;
       for(var i of data.items) {
         res.push({
           type: 'github',
