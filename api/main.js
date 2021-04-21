94912bf35a0c22bec27d6590be98ff5c05953537
liimee <alt3753.7@gmail.com>
2021-04-22 06:30:37 +0700

    😓

--- api/main.js
+++ api/main.js
@@ -1,13 +1,13 @@
 export default (req) => {
   var res = [];
 
-  fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${decodeURIComponent(req.url.split('?search=')[1])}&per_page=100`)}`)
+  fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${decodeURIComponent(req.url.split('?search=')[1])}&per_page=100`)}`)
     .then(response => {
       if (response.ok) return response.json()
       throw new Error('Network response was not ok.')
     })
     .then(data => {
-      data.contents.items.forEach(i => {
+      data.items.forEach(i => {
         res.push({
           type: 'github',
           title: i.full_name,
