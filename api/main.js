f27b374c1999202a934a2a5f055a38a27eb6bbaa
liimee <alt3753.7@gmail.com>
2021-04-22 05:48:41 +0700

    f

--- api/main.js
+++ api/main.js
@@ -6,7 +6,19 @@ export default (req) => {
       if (response.ok) return response.json()
       throw new Error('Network response was not ok.')
     })
-    .then(data => req.respond({ body: data.contents }));
+    .then(data => {
+      data = data.contents;
+      data.items.forEach(i => {
+        res.push({
+          type: 'github',
+          title: i.full_name,
+          desc: i.description || 'No description available.',
+          link: i.html_url,
+          site: 'GitHub'
+        })
+      })
+      req.respond({ body: res })
+    });
 
   // GitHub
   /* fetch(`https://api.github.com/search/repositories?q=${decodeURIComponent(req.url.split('?search=')[1])}&per_page=100`)
