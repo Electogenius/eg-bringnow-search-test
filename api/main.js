0f1acc85ef551c0d89fa0ff1451f9570293943aa
liimee <70614549+liimee@users.noreply.github.com>
2021-04-21 23:07:06 +0700

    Update main.js

--- api/main.js
+++ api/main.js
@@ -1,3 +1,22 @@
 export default (req) => {
-  req.respond({ body: decodeURIComponent(req.url.split('?search=')[1]) });
+  var res = [];
+
+  // GitHub
+  fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${decodeURIComponent(req.url.split('?search=')[1])}&per_page=100`)
+    .then(response => {
+      if (response.ok) return response.text()
+      throw new Error('err')
+    }).then((a) => {
+      var data = JSON.parse(a.responseText);
+      for(var i of data.items) {
+        res.push({
+          type: 'github',
+          title: i.full_name,
+          desc: i.description||'No description available.',
+          link: i.html_url,
+          site: 'GitHub'
+        });
+      }
+      req.respond({ body: res });
+    });
 };
