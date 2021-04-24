52f55a6889bf16ef7a4e69de0c2f84fd596214fa
liimee <alt3753.7@gmail.com>
2021-04-24 17:08:30 +0700

    test

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ addEventListener("fetch", (event) => {
 });
 
 async function geth(s) {
-  var u = new URL(s.request.url).search.substring(1);
+  var u = new URL(s.request.url).search.substring(1); //search
+  
+  // GitHub
   var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${u}&per_page=100`)}`)
   if (t.ok) {
     var data = await t.json();
@@ -16,12 +18,29 @@ async function geth(s) {
         site: 'GitHub'
       })
     });
-    return new Response(
+  } 
+  
+  //Gitlab
+  t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://gitlab.com/api/v4/projects?search=${u}`)}`);
+  if(t.ok) {
+    var data = await t.json();
+    data.forEach(v => {
+      res.push({
+        title: v.name_with_namespace,
+        desc: v.description || 'No description available.',
+        link: v.web_url,
+        site: 'GitLab'
+      })
+    });
+  }
+  
+  res = arr.sort(() => Math.random() - 0.5);
+  
+  return new Response(
       JSON.stringify(res),
       {
         headers: {
           "content-type": "text/plain; charset=UTF-8"
         }
       });
-  }
 }
