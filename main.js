44fbeb5c6f659bbeeab7ed42ffd9384820a71cdf
liimee <alt3753.7@gmail.com>
2021-04-24 17:17:59 +0700

    hmm

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ async function geth(s) {
   var u = new URL(s.request.url).search.substring(1); //search
 
   // GitHub
-  var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${u}&per_page=100`)}`)
+  /*var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${u}&per_page=100`)}`)
   if (t.ok) {
     var data = await t.json();
     data.items.forEach(v => {
@@ -18,20 +18,10 @@ async function geth(s) {
         site: 'GitHub'
       })
     });
-    
-      res = arr.sort(() => Math.random() - 0.5);
-
-    return new Response(
-      JSON.stringify(res),
-      {
-        headers: {
-          "content-type": "text/plain; charset=UTF-8"
-        }
-      });
-  }
+  }*/
 
   //Gitlab
- /* t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://gitlab.com/api/v4/projects?search=${u}`)}`);
+  t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://gitlab.com/api/v4/projects?search=${u}`)}`);
   if (t.ok) {
     var data = await t.json();
     data.forEach(v => {
@@ -41,10 +31,16 @@ async function geth(s) {
         link: v.web_url,
         site: 'GitLab'
       })
-    });*/
-
-
+    });
+  }
   
-//  }
-
+  res = arr.sort(() => Math.random() - 0.5);
+  
+  return new Response(
+      JSON.stringify(res),
+      {
+        headers: {
+          "content-type": "text/plain; charset=UTF-8"
+        }
+      });
 }
