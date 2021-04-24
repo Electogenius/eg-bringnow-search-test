ec29696d0096fc4fd603c6ac93d0bc0857e45ce8
liimee <alt3753.7@gmail.com>
2021-04-24 21:26:59 +0700

    j

--- api/index.js
+++ api/index.js
@@ -3,9 +3,8 @@ const fetch = require('node-fetch');
 const app = express();
 var res = [];
 
-app.get('/:se', async (req, re) => {
-  const {se} = req.params;
-  //const se = new URL(req.protocol + '://' + req.get('host') + req.originalUrl).searchParams.get('search')
+app.get('/', async (req, re) => {
+  const se = req.query.search;
   var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${se}&per_page=100`)}`)
   if (t.ok) {
     var data = await t.json();
