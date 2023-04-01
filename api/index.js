20b6acb82fee53716ebafe70a672dba1ad12e513
ta <alt3753.7@gmail.com>
2023-04-01 16:45:02 +0700

    idk

--- api/index.js
+++ api/index.js
@@ -1,48 +1,66 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-var res = [];
 
-app.get('/api', async (req, re) => {
-  const se = req.query.search;
+async function gh(se) {
   var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.github.com/search/repositories?q=${se}&per_page=20`)}`)
   if (t.ok) {
     var data = await t.json();
-    data.items.forEach(v => {
-      res.push({
+    return data.items.map(v => {
+      return {
         title: v.full_name,
         desc: v.description || 'No description available.',
         link: v.html_url,
         site: 'GitHub'
-      })
+      }
     });
   }
+}
+
+async function gitlab(se) {
   var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://gitlab.com/api/v4/projects?search=${se}`)}`)
   if (t.ok) {
     var data = await t.json();
-    data.forEach(v => {
-      res.push({
+    return data.map(v => {
+      return {
         title: v.name_with_namespace,
         desc: v.description || 'No description available.',
         link: v.web_url,
         site: 'GitLab'
-      })
-    });
-    res.sort(function(a, b){return 0.5 - Math.random()});
-    re.end(JSON.stringify(res))
+      }
+    })
   }
+}
+
+async function npm(se) {
   var t = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.npms.io/v2/search?q=${se}`)}`)
-  if(t.ok) {
+  if (t.ok) {
     var data = await t.json();
-    data.results.forEach(v => {
-      res.push({
+    return data.results.map(v => {
+      return {
         title: v.package.name,
         desc: v.package.description || 'No description available',
         link: 'https://npmjs.com/package/' + v.package.name,
         site: 'NPM Registry'
-      })
+      }
     })
   }
+}
+
+app.get('/api', (req, re) => {
+  const se = req.query.search;
+
+  Promise.all([
+    gh(se),
+    gitlab(se),
+    npm(se)
+  ]).then(v => {
+    const res = v.flat().sort(() => 0.5 - Math.random());
+
+    re.end(JSON.stringify(res));
+  })
 })
+
+app.listen(3000)
 
 module.exports = app;
